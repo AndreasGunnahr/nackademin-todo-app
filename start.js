c490e1eb8352588a2a60bb3fd3ab492477590387
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2020-09-16 14:34:41 +0200

    add create script in our express json for our react app

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 const dbConnect = require("./database");
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listen on port ${process.env.PORT}`);
 });
